24b9b8bd7b972ded7926aa0c598777ff918d8ba6
TheOnion313 <royn77777@gmail.com>
2020-11-08 16:32:35 +0200

    Added slider pointing to generations per second, also added Reset button without realizing im still on the feature branch

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,38 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
 public class Main extends Application {
 
-    Button button;
+    static Board b;
     public static HBox board;
     static Stage window;
     public static boolean pause = true;
     public static final int WIDTH = 20,
             HEIGHT = 20,
-            CYCLE = 100;
+            MIN_PACE = 10,
+            MAX_PACE = 1000,
+            DEFAULT_PACE = MAX_PACE,
+            CONTROL_SPACING = 20;
+    public static int CYCLE = MAX_PACE - DEFAULT_PACE + MIN_PACE;
     public static long lastUpdate = System.currentTimeMillis() - CYCLE;
 
 
@@ -38,14 +47,12 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("Conway's Game Of Life");
 
-        Board b = new Board(WIDTH, HEIGHT);
+        b = new Board(WIDTH, HEIGHT);
 
         board = new HBox();
         b.show(board);
 
         Button cycle = new Button();
-        cycle.setMinHeight(20);
-        cycle.setMaxHeight(20);
         cycle.setText("Resume");
         cycle.setOnAction(e -> {
             Main.pause = !Main.pause;
@@ -53,21 +60,40 @@ public class Main extends Application {
             lastUpdate = System.currentTimeMillis() - CYCLE - 1;
         });
 
+        Slider pace = new Slider(MIN_PACE, MAX_PACE, DEFAULT_PACE);
+        pace.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                CYCLE = (int) (MAX_PACE - pace.getValue() + MIN_PACE);
+            }
+        });
+
+        Button reset = new Button();
+        reset.setText("Reset");
+        reset.setOnAction(e -> {
+            b = new Board(WIDTH, HEIGHT);
+            b.show(board);
+        });
+
         VBox main = new VBox();
         main.setSpacing(10);
         main.setAlignment(Pos.CENTER);
 
-        main.getChildren().addAll(cycle, board);
+        HBox control = new HBox(new Rectangle(0, 0), cycle, pace, reset);
+        control.setSpacing(CONTROL_SPACING);
 
-        int width = Cell.SIZE * WIDTH;
-        int height = Cell.SIZE * HEIGHT  + (int)(10 * main.getSpacing());
+        main.getChildren().addAll(control, board);
+
+        int width = Board.SPACING + (Cell.SIZE + Board.SPACING) * WIDTH;
+        int height = Cell.SIZE * HEIGHT + (int) (10 * main.getSpacing());
         Scene scene = new Scene(main, width, height);
 
         window.setScene(scene);
 
         Timeline timeline =
-                new Timeline(new KeyFrame(Duration.millis(CYCLE + 10), e -> {
-                    if (!pause && (System.currentTimeMillis() - lastUpdate) - CYCLE >= 0) {
+                new Timeline(new KeyFrame(Duration.millis(MIN_PACE), e -> {
+
+                    if (!pause && System.currentTimeMillis() - lastUpdate - CYCLE >= 0) {
                         b.update();
                         b.show(board);
                         lastUpdate = System.currentTimeMillis();
